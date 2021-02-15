cc0b43accb0c432815311924d5d2cea0633ab355
alicanerdurmaz <alicanerdurmaz@gmail.com>
2021-02-15 15:03:14 +0300

    img onerror, show placeholder

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ class MoviesApp {
 
   createMovieEl(movie) {
     const { image, title, genre, year, id } = movie
-    return `<tr data-id="${id}"><td><img src="${image}"></td><td>${title}</td><td>${genre}</td><td>${year}</td></tr>`
+    return `<tr data-id="${id}"><td><img src="${image}" onerror="this.src='./assets/img/ImagePlaceholder.png'"></td><td>${title}</td><td>${genre}</td><td>${year}</td></tr>`
   }
 
   fillTable() {
